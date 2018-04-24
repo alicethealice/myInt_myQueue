4c87e25f8afd68a83f82fc16293939582f446017
alicethealice <38686594+alicethealice@users.noreply.github.com>
2018-04-24 13:25:40 +0400

    Add files via upload

---
+++ myQueue/MaQ/DEQueue.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaQ
+{
+    public class Node<T>
+    {
+        public Node(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+        public Node<T> Prev{ get; set; }
+    }
+
+    public class DEQueue<T>
+    {
+        
+        Node<T> first;
+        Node<T> last; 
+        int count;
+
+        public void pushBack(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            Node<T> temp = last;
+           last= node;
+            if (count == 0)
+                first = last;
+            else
+            {
+                temp.Next = last;
+                last.Prev = temp;
+            }
+            count++;
+        }
+
+        public void pushFront(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            Node<T> temp = first;
+            first = node;
+            if (count == 0)
+                last = first;
+            else
+            {
+                temp.Prev = first;
+                first.Next = temp;
+            }
+            count++;
+        }
+
+        public T front()
+        {
+            if (count != 0)
+            {
+                return first.Data;
+            }
+            else
+                throw new InvalidOperationException();
+        }
+        public T back()
+        {
+            if (count != 0)
+            {
+                return last.Data;
+            }
+            else
+                throw new InvalidOperationException();
+        }
+        public int size() {
+            return count;
+        }
+        public void clear() {
+            count = 0;
+        }
+        public T popFront()
+        {
+            if (count == 0)
+                throw new InvalidOperationException();
+            T output = first.Data;
+            first = first.Next;
+            count--;
+            return output;
+        }
+        public T popBack()
+        {
+            if (count == 0)
+                throw new InvalidOperationException();
+            T output = last.Data;
+            last = last.Prev;
+            count--;
+            return output;
+        }
+
+
+        public T[] toArray()
+        {
+            if (count == 0)
+                throw new InvalidOperationException();
+            T[] output = new T[count];
+            Node<T> temp = first;
+            for (int i = 0; i < count; i++) {
+                output[i] = temp.Data;
+                temp = temp.Next;
+            }
+            return output;
+        }
+
+
+
+    }
+}
---
+++ myQueue/MaQTest/DEQueueTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MaQ;
+namespace MaQTest
+{
+    [TestClass]
+    public class DEQueueTest
+    {
+
+        [TestMethod]
+        public void testFront_mast_odin_dva_tri()
+        {
+            DEQueue<string> qwsda=new DEQueue<string>();
+            qwsda.pushBack("KAte");
+            qwsda.pushBack("LOLQOP");
+            Assert.AreEqual(qwsda.front(), "KAte");
+        }
+
+        [TestMethod]
+        public void test_popfront_a_b_c_d()
+        {
+            DEQueue<string> qwsda = new DEQueue<string>();
+            qwsda.pushBack("a");
+            qwsda.pushBack("b");
+            qwsda.pushBack("c");
+            qwsda.pushBack("d");
+            string a = qwsda.popFront() + qwsda.popFront() + qwsda.popFront()+ qwsda.popFront();
+            Assert.AreEqual(a, "abcd");
+        }
+        [TestMethod]
+        public void test_popback_d_c_b_a()
+        {
+            DEQueue<string> qwsda = new DEQueue<string>();
+            qwsda.pushBack("a");
+            qwsda.pushBack("b");
+            qwsda.pushBack("c");
+            qwsda.pushBack("d");
+            string a = qwsda.popBack() + qwsda.popBack() + qwsda.popBack() + qwsda.popBack();
+            Assert.AreEqual(a, "dcba");
+        }
+
+        [TestMethod]
+        public void test_popfront_popback_a_d_b_c()
+        {
+            DEQueue<string> qwsda = new DEQueue<string>();
+            qwsda.pushBack("a");
+            qwsda.pushBack("b");
+            qwsda.pushBack("c");
+            qwsda.pushBack("d");
+            string a = qwsda.popFront() + qwsda.popBack() + qwsda.popFront() + qwsda.popBack();
+            Assert.AreEqual(a, "adbc");
+        }
+
+        [TestMethod]
+        public void test_toArray_a_d_b_c()
+        {
+            DEQueue<int> qwsda = new DEQueue<int>();
+            qwsda.pushBack(1);
+            qwsda.pushBack(2);
+            qwsda.pushBack(3);
+            qwsda.pushBack(4);
+            int[] a = new int[4];
+            a[0] = 1;a[1] = 2;a[2] = 3;a[3] = 4;
+            int[] b = qwsda.toArray();
+            //Assert.AreEqual(a, b);
+            CollectionAssert.AreEqual(a, b);
+        }
+
+
+    }
+}
